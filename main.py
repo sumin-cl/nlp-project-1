8448375251bd8edb455500f86483cf1ebcb686ee
Sumin Choe <mi.choe@campus.lmu.de>
2026-01-07 10:15:53 +0100

    feat: add text complexity analysis module using Flesch index

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ nltk.download('punkt_tab')
 import json
 import argparse
 
+from src.readability import flesch_simple_check
+
 def dict_to_json(input, indent=4, sort_keys=False):
     """
     Convert a Python dictionary to a JSON-formatted string.
@@ -17,15 +19,22 @@ def dict_to_json(input, indent=4, sort_keys=False):
     json_str = json.dumps(input, indent=indent, sort_keys=sort_keys, ensure_ascii=False)
     return json_str
 
-def run_cli(input_filepath, output_filepath):
+def run_cli(input_filepath, output_filepath, task):
     """Liest von input_filepath, analysiert und schreibt nach output_filepath"""
 
     input_text = load_text(input_filepath)
 
     tokens = preprocess_text(input_text)
     print(tokens)
+    
+    if task == 'korean':
 
-    output_data = analyze_text(input_text)
+        output_data = analyze_text(input_text)
+
+    elif task == 'readability':
+        readability_score = flesch_simple_check(input_text)
+
+        output_data = readability_score
 
     with open(output_filepath, 'w', encoding='utf-8') as output_file:
         json.dump(output_data, output_file, ensure_ascii = False, indent = 4, sort_keys = False)
@@ -39,6 +48,9 @@ if __name__ == "__main__":
     parser.add_argument('input_filepath', default='data/okm_sample.txt', help='Pfad zur Input-Textdatei (default: data/okm_sample.txt).')
 
     parser.add_argument('-o', '--output', default='data/result.json', help='Pfad für die JSON-Datei (default: data/result.json)')
+    
+    parser.add_argument('--task', choices=['korean', 'readability'], default='korean', help='Choose analysis mode')
+    
     args = parser.parse_args()
 
-    run_cli(args.input_filepath, args.output)
+    run_cli(args.input_filepath, args.output, args.task)
---
+++ src/readability.py
@@ -0,0 +1,16 @@
+import textstat
+
+def flesch_simple_check(txt):
+    # Flesch Reading Ease Metrik
+    score = textstat.flesch_reading_ease(txt)
+
+    print (f"Flesch Score: {score}")
+
+    level = "Standard"
+    if score > 90: level = "Very Easy"
+    if score < 90: level = "Easy"
+    if score < 70: level = "Standard"
+    if score < 50: level = "Difficult"
+    elif score < 30: level = "Confusing"
+
+    return {"score": score, "level": level}
